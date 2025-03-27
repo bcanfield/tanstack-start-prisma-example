f8c5326d085c9d21d4bbee526f8ae8e344f85c6c
Brandin <brandin.canfield.ctr@us.af.mil>
2025-03-27 11:12:32 -0400

    Got prisma working

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,31 +1,22 @@
-import * as fs from "node:fs";
 import { createFileRoute, useRouter } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/react-start";
+import { PrismaClient } from "@prisma/client";
 
-const filePath = "count.txt";
+const prisma = new PrismaClient();
 
-async function readCount() {
-  return parseInt(
-    await fs.promises.readFile(filePath, "utf-8").catch(() => "0")
-  );
-}
-
-const getCount = createServerFn({
-  method: "GET",
-}).handler(() => {
-  return readCount();
+const getPosts = createServerFn({ method: "GET" }).handler(async () => {
+  return prisma.post.findMany();
 });
 
-const updateCount = createServerFn({ method: "POST" })
-  .validator((d: number) => d)
+const createPost = createServerFn({ method: "POST" })
+  .validator((d: { title: string; content: string }) => d)
   .handler(async ({ data }) => {
-    const count = await readCount();
-    await fs.promises.writeFile(filePath, `${count + data}`);
+    await prisma.post.create({ data });
   });
 
 export const Route = createFileRoute("/")({
   component: Home,
-  loader: async () => await getCount(),
+  loader: async () => await getPosts(),
 });
 
 function Home() {
@@ -33,15 +24,24 @@ function Home() {
   const state = Route.useLoaderData();
 
   return (
-    <button
-      type="button"
-      onClick={() => {
-        updateCount({ data: 1 }).then(() => {
-          router.invalidate();
-        });
-      }}
-    >
-      Add 1 to {state}?
-    </button>
+    <div>
+      <button
+        onClick={() => {
+          createPost({ data: { title: "Hello", content: "World" } }).then(
+            () => {
+              router.invalidate();
+            }
+          );
+        }}
+      >
+        Create Post
+      </button>
+      <h1>Posts</h1>
+      <ul>
+        {state.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
